4b1818bccd85a8a1fc45fcf4b784869e9caf61a7
huhsiyoung <july433@naver.com>
2025-11-26 15:03:24 +0900

    first commit

---
+++ notification-service/src/test/java/com/destiny/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destiny.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ stock-service/src/test/java/com/destiny/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destiny.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
