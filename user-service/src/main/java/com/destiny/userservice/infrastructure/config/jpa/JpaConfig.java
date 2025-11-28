a1af3f2dc430b7f245da4fdf8208925664106342
jieun <dlwldms0895@gmail.com>
2025-11-28 14:19:01 +0900

    chore : jpaAuditing 추가

---
+++ user-service/src/main/java/com/destiny/userservice/infrastructure/config/jpa/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.destiny.userservice.infrastructure.config.jpa;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
