4d5bf4fe1089c72d7de048c9ed2d1dac223b17c7
changhui98 <net_0102@naver.com>
2025-11-28 18:17:26 +0900

    Feat : 초기 주문 요청 기능 구현

--- order-service/src/main/java/com/destiny/orderservice/domain/repository/OrderRepository.java
+++ order-service/src/main/java/com/destiny/orderservice/domain/repository/OrderRepository.java
@@ -1,5 +1,9 @@
 package com.destiny.orderservice.domain.repository;
 
+import com.destiny.orderservice.domain.entity.Order;
+import java.util.UUID;
+
 public interface OrderRepository {
 
+    Order createOrder(Order order);
 }
