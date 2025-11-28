892a3f692dda3bfe43ae9f80af8ad8ffc8ae3686
wnsgud0310 <ffjjo0310@naver.com>
2025-11-28 14:18:40 +0900

    feat: 쿠폰템플릿 단건 조회 기능 구현

---
+++ coupon-service/src/main/java/com/destiny/couponservice/presentation/dto/response/CouponTemplateGetResponse.java
@@ -0,0 +1,36 @@
+package com.destiny.couponservice.presentation.dto.response;
+
+
+import com.destiny.couponservice.domain.enums.DiscountType;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CouponTemplateGetResponse {
+
+
+    private UUID id;
+
+    private String code;
+    private String name;
+
+    private DiscountType discountType;
+    private Integer discountValue;
+
+    private Integer minOrderAmount;
+
+    private Boolean isDuplicateUsable;
+
+    private Integer maxDiscountAmount;
+    private Integer dailyIssueLimit;
+    private Integer perUserTotalLimit;
+
+    private LocalDateTime availableFrom;
+    private LocalDateTime availableTo;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
