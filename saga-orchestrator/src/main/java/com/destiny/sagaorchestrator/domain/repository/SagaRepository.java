e4240d912bbf92025d1d260a2a686af2c65b73ac
changhui98 <net_0102@naver.com>
2025-11-29 16:57:12 +0900

    Feat : 사가 객체 생성 기능 초기 세팅

--- saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/domain/repository/SagaRepository.java
+++ saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/domain/repository/SagaRepository.java
@@ -1,5 +1,8 @@
 package com.destiny.sagaorchestrator.domain.repository;
 
+import com.destiny.sagaorchestrator.domain.entity.SagaState;
+
 public interface SagaRepository {
 
+    void createSaga(SagaState saga);
 }
