5b1b764e0d7579887ceed71a35b8680023d2c4c0
wnsgud0310 <ffjjo0310@naver.com>
2025-11-30 16:19:52 +0900

    feat: 쿠폰 템플릿 수정 기능 추가

--- coupon-service/src/main/java/com/destiny/couponservice/domain/entity/CouponTemplate.java
+++ coupon-service/src/main/java/com/destiny/couponservice/domain/entity/CouponTemplate.java
@@ -2,11 +2,21 @@ package com.destiny.couponservice.domain.entity;
 
 import com.destiny.couponservice.domain.enums.DiscountType;
 import com.destiny.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -53,4 +63,49 @@ public class CouponTemplate extends BaseEntity {
 
     @Column(nullable = false)
     private LocalDateTime availableTo;
+
+
+    public void update(
+        String name,
+        DiscountType discountType,
+        Integer discountValue,
+        Integer minOrderAmount,
+        Boolean isDuplicateUsable,
+        Integer maxDiscountAmount,
+        Integer dailyIssueLimit,
+        Integer perUserTotalLimit,
+        LocalDateTime availableFrom,
+        LocalDateTime availableTo
+    ) {
+        if (name != null) {
+            this.name = name;
+        }
+        if (discountType != null) {
+            this.discountType = discountType;
+        }
+        if (discountValue != null) {
+            this.discountValue = discountValue;
+        }
+        if (minOrderAmount != null) {
+            this.minOrderAmount = minOrderAmount;
+        }
+        if (isDuplicateUsable != null) {
+            this.isDuplicateUsable = isDuplicateUsable;
+        }
+        if (maxDiscountAmount != null) {
+            this.maxDiscountAmount = maxDiscountAmount;
+        }
+        if (dailyIssueLimit != null) {
+            this.dailyIssueLimit = dailyIssueLimit;
+        }
+        if (perUserTotalLimit != null) {
+            this.perUserTotalLimit = perUserTotalLimit;
+        }
+        if (availableFrom != null) {
+            this.availableFrom = availableFrom;
+        }
+        if (availableTo != null) {
+            this.availableTo = availableTo;
+        }
+    }
 }
