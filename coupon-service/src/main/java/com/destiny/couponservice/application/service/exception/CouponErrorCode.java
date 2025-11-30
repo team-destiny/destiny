e88f07cf6d9b157a2c7ed936bb039a7493b5cb57
wnsgud0310 <ffjjo0310@naver.com>
2025-12-01 00:21:05 +0900

    refactor: 에러 코드 오타 수정

--- coupon-service/src/main/java/com/destiny/couponservice/application/service/exception/CouponErrorCode.java
+++ coupon-service/src/main/java/com/destiny/couponservice/application/service/exception/CouponErrorCode.java
@@ -9,7 +9,7 @@ public enum CouponErrorCode implements ResponseCode {
     TEMPLATE_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON-002", "쿠폰 템플릿을 찾을 수 없습니다."),
     INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "COUPON-003", "종료일은 시작일 이후여야 합니다."),
     INVALID_DISCOUNT_VALUE(HttpStatus.BAD_REQUEST, "COUPON-004", "정률 할인 값은 1~100 사이여야 합니다."),
-    MISSING_MAX_DISCOUNT(HttpStatus.BAD_REQUEST, "COUPON -005", "정률 할인 시 최대 할인 금액은 필수입니다.");
+    MISSING_MAX_DISCOUNT(HttpStatus.BAD_REQUEST, "COUPON-005", "정률 할인 시 최대 할인 금액은 필수입니다.");
 
 
     private final HttpStatus status;
