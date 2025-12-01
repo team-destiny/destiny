3495bdf386bf79bdaef7f47bff65368b889d7fe8
jieun <dlwldms0895@gmail.com>
2025-12-02 04:05:31 +0900

    style : 폴더 구조 변경

--- user-service/src/main/java/com/destiny/userservice/infrastructure/security/config/PasswordEncoderConfig.java
+++ user-service/src/main/java/com/destiny/userservice/infrastructure/security/config/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package com.destiny.userservice.infrastructure.config;
+package com.destiny.userservice.infrastructure.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
