9181454b2277044564b55bd8e906b9887c755f4c
changhui98 <net_0102@naver.com>
2025-12-01 23:26:09 +0900

    Feat : 사가 서비스에서 상품 저장할 수 있도록 기능 추가

--- saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/application/service/SagaService.java
+++ saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/application/service/SagaService.java
@@ -3,6 +3,7 @@ package com.destiny.sagaorchestrator.application.service;
 import com.destiny.sagaorchestrator.domain.entity.SagaState;
 import com.destiny.sagaorchestrator.domain.repository.SagaRepository;
 import com.destiny.sagaorchestrator.infrastructure.messaging.event.request.OrderCreateRequestEvent;
+import com.destiny.sagaorchestrator.infrastructure.messaging.event.result.ProductValidateResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,16 +19,57 @@ public class SagaService {
     @Transactional
     public void createSaga(OrderCreateRequestEvent event) {
 
+        // 1) 사가 초기 생성
         SagaState saga = SagaState.of(
             event.orderId(),
-            event.userId()
+            event.userId(),
+            event.couponId()
         );
 
-        // TODO : 상품 검증 이밴트 발행
+        // 2) productId 기반으로 빈 ProductValidateResult 등록
         event.items().forEach(item -> {
-
+            saga.getProductResults().put(
+                item.productId(),
+                // 아직 검증 전이므로 null값
+                null
+            );
         });
 
         sagaRepository.createSaga(saga);
+        /*
+        // 4) 상품 검증 이벤트 발행
+        event.items().forEach(item -> {
+            productProducer.send(
+                item.productId(),
+                item.itemPromotionId(),
+                item.stock()
+            );
+        });
+         */
+    }
+
+    // TODO : 상품 서비스 검증 및 상품 가격 가지고 오기
+    @Transactional
+    public void productValidate(ProductValidateResult result) {
+
+
+    }
+
+    // TODO : 쿠폰 검증 및 쿠폰 할인율 가지고 오기
+    @Transactional
+    public void couponValidate() {
+
+    }
+
+    // TODO : 재고 차감
+    @Transactional
+    public void stockUpdate() {
+
+    }
+
+    // TODO : 결제
+    @Transactional
+    public void paymentOrder() {
+
     }
 }
