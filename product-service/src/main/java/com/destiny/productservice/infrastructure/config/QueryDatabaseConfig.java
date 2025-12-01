665d5c3dc5597e3c3552b065cce37a83275eb24e
huhsiyoung <july433@naver.com>
2025-12-01 16:22:31 +0900

    feat: 상품 조회 데이터베이스 설정 추가

---
+++ product-service/src/main/java/com/destiny/productservice/infrastructure/config/QueryDatabaseConfig.java
@@ -0,0 +1,44 @@
+package com.destiny.productservice.infrastructure.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableJpaRepositories(
+    basePackages = "com.destiny.productservice.infrastructure.repository.query",
+    entityManagerFactoryRef = "queryEntityManagerFactory",
+    transactionManagerRef = "queryTransactionManager"
+)
+public class QueryDatabaseConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.query")
+    public DataSource queryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean queryEntityManagerFactory(
+        EntityManagerFactoryBuilder builder) {
+        return builder
+            .dataSource(queryDataSource())
+            .packages("com.destiny.productservice.infrastructure.repository.query")
+            .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager queryTransactionManager(
+        @Qualifier("queryEntityManagerFactory") EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+}
