fb4ef4f14bf3121ab137215fa007a05656df0211
jieun <dlwldms0895@gmail.com>
2025-12-01 14:53:17 +0900

    fix : global 빈 스캔 (global Exception 적용)

--- user-service/src/main/java/com/destiny/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/destiny/userservice/UserServiceApplication.java
@@ -3,7 +3,10 @@ package com.destiny.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.destiny.userservice",
+    "com.destiny.global" // global 빈 스캔
+})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
