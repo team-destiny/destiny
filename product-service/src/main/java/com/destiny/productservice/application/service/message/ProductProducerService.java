df9330c9b9f7a20670af6b6b0c8c3e3f80e0cd1a
huhsiyoung <july433@naver.com>
2025-12-01 16:25:03 +0900

    feat: product producer 서비스 추가

---
+++ product-service/src/main/java/com/destiny/productservice/application/service/message/ProductProducerService.java
@@ -0,0 +1,22 @@
+package com.destiny.productservice.application.service.message;
+
+import com.destiny.productservice.application.dto.ProductMessage;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductProducerService {
+
+    private final KafkaTemplate<UUID, ProductMessage> kafkaTemplate;
+
+    public void sendProductCreateAfterEvent(UUID key, ProductMessage productMessage) {
+        kafkaTemplate.send("product_after_create", key, productMessage);
+    }
+
+    public void sendProductUpdateAfterEvent(UUID key, ProductMessage productMessage) {
+        kafkaTemplate.send("product_after_update", key, productMessage);
+    }
+}
