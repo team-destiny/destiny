de1cd76103308a7f2e20c949851cddc74dd0f661
changhui98 <net_0102@naver.com>
2025-12-01 23:28:01 +0900

    Refactor : 브랜드 서비스 불필요한 import 제거

--- brand-service/src/main/java/com/destiny/brandservice/domain/repository/BrandRepository.java
+++ brand-service/src/main/java/com/destiny/brandservice/domain/repository/BrandRepository.java
@@ -1,7 +1,6 @@
 package com.destiny.brandservice.domain.repository;
 
 import com.destiny.brandservice.domain.entity.Brand;
-import com.destiny.brandservice.presentation.dto.response.BrandResponse;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
