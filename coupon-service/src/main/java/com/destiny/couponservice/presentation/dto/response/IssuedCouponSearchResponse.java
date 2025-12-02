5ac031398d83e0274db5e80943b4513d35f43f12
wnsgud0310 <ffjjo0310@naver.com>
2025-12-02 14:54:34 +0900

    feat: 발행된 쿠폰 dto 추가

---
+++ coupon-service/src/main/java/com/destiny/couponservice/presentation/dto/response/IssuedCouponSearchResponse.java
@@ -0,0 +1,28 @@
+package com.destiny.couponservice.presentation.dto.response;
+
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+@Getter
+@Builder
+public class IssuedCouponSearchResponse {
+
+    private List<IssuedCouponResponseDto> contents;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+
+    public static IssuedCouponSearchResponse from(Page<IssuedCouponResponseDto> page) {
+        return IssuedCouponSearchResponse.builder()
+            .contents(page.getContent())
+            .page(page.getNumber())
+            .size(page.getSize())
+            .totalElements(page.getTotalElements())
+            .totalPages(page.getTotalPages())
+            .build();
+    }
+}
