4972257ea9ee7a8b4a30fb43ad2301734b1f1bad
jieun <dlwldms0895@gmail.com>
2025-12-02 16:21:55 +0900

    feat : jwt 토큰 검증 필터 구현

---
+++ gateway/src/main/java/com/destiny/gateway/infrastructure/security/filter/AccessTokenValidationFilter.java
@@ -0,0 +1,95 @@
+package com.destiny.gateway.infrastructure.security.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.destiny.gateway.infrastructure.security.jwt.JwtProperties;
+import com.destiny.gateway.presentation.advice.GatewayErrorCode;
+import com.destiny.gateway.presentation.advice.GatewayException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AccessTokenValidationFilter implements GlobalFilter, Ordered {
+
+    private final JwtProperties jwtProperties;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        String accessJwt = resolveAccessToken(authorizationHeader);
+
+        log.info("Access Token 검증 시작: {}", accessJwt);
+        
+        if (!StringUtils.hasText(accessJwt)) {
+            return chain.filter(removeAuthorizationHeaderFromExchange(exchange));
+        }
+
+        DecodedJWT decodedAccessJwt;
+        try {
+            decodedAccessJwt = JWT.require(Algorithm.HMAC256(jwtProperties.getSecretKey()))
+                                .build()
+                                .verify(accessJwt);
+        } catch (JWTVerificationException e) {
+            throw new GatewayException(GatewayErrorCode.GATEWAY_TOKEN_INVALID);
+        } catch (IllegalArgumentException e) {
+            throw new GatewayException(GatewayErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+        String id = decodedAccessJwt.getClaim("id").asString();
+        if (!StringUtils.hasText(id)) {
+            throw new GatewayException(GatewayErrorCode.GATEWAY_TOKEN_INVALID);
+        }
+
+        // TODO : 토큰 블랙리스트 검증 추가
+        
+        return chain.filter(exchange);
+    }
+
+    /**
+     * AccessToken 추출
+     */
+    private String resolveAccessToken(String authorizationHeader) {
+        if (!StringUtils.hasText(authorizationHeader)) {
+            return null;
+        }
+
+        if (authorizationHeader.regionMatches(true, 0, jwtProperties.getHeaderPrefix(), 0, jwtProperties.getHeaderPrefix().length())) {
+            return authorizationHeader.substring(jwtProperties.getHeaderPrefix().length()).trim();
+        }
+        return authorizationHeader.trim();
+    }
+
+    /**
+     * 토큰 없는 경우 익명 요청 처리 (Authorization 헤더 제거)
+     */
+    private static ServerWebExchange removeAuthorizationHeaderFromExchange(ServerWebExchange exchange) {
+        log.info("토큰 헤더 제거");
+        ServerHttpRequest mutatedRequest = exchange.getRequest()
+            .mutate()
+            .headers(headers -> headers.remove(HttpHeaders.AUTHORIZATION))
+            .build();
+        ServerWebExchange mutatedExchange = exchange.mutate()
+            .request(mutatedRequest)
+            .build();
+        log.info("토큰 헤더 제거 완료");
+        return mutatedExchange;
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
