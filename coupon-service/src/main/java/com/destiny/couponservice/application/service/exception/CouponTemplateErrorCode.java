5ab466a45bc80b23b7143e2e0cd4c5265dcacd75
wnsgud0310 <ffjjo0310@naver.com>
2025-12-02 14:51:51 +0900

    rename: 쿠폰 템플릿 에러코드 파일이름 변경

--- coupon-service/src/main/java/com/destiny/couponservice/application/service/exception/CouponTemplateErrorCode.java
+++ coupon-service/src/main/java/com/destiny/couponservice/application/service/exception/CouponTemplateErrorCode.java
@@ -4,7 +4,7 @@ import com.destiny.global.code.ResponseCode;
 import lombok.Generated;
 import org.springframework.http.HttpStatus;
 
-public enum CouponErrorCode implements ResponseCode {
+public enum CouponTemplateErrorCode implements ResponseCode {
     DUPLICATE_TEMPLATE_CODE(HttpStatus.CONFLICT, "COUPON-001", "이미 존재하는 쿠폰 코드입니다."),
     TEMPLATE_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON-002", "쿠폰 템플릿을 찾을 수 없습니다."),
     INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "COUPON-003", "종료일은 시작일 이후여야 합니다."),
@@ -31,7 +31,7 @@ public enum CouponErrorCode implements ResponseCode {
         return this.message;
     }
 
-    CouponErrorCode(HttpStatus status, String code, String message) {
+    CouponTemplateErrorCode(HttpStatus status, String code, String message) {
         this.status = status;
         this.code = code;
         this.message = message;
