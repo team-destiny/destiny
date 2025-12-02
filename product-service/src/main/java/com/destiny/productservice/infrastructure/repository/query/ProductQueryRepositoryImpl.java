0b325fc3115040aed267d591bab9ea8a121dd034
huhsiyoung <july433@naver.com>
2025-12-02 20:22:48 +0900

    feat: elasticsearch 기반 조회 모델 추가

--- product-service/src/main/java/com/destiny/productservice/infrastructure/repository/query/ProductQueryRepositoryImpl.java
+++ product-service/src/main/java/com/destiny/productservice/infrastructure/repository/query/ProductQueryRepositoryImpl.java
@@ -2,37 +2,24 @@ package com.destiny.productservice.infrastructure.repository.query;
 
 import com.destiny.productservice.domain.entity.ProductView;
 import com.destiny.productservice.domain.repository.ProductQueryRepository;
-import com.destiny.productservice.presentation.dto.request.ProductSearch;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class ProductQueryRepositoryImpl implements ProductQueryRepository {
 
-    private final ProductQueryJpaRepository jpaRepository;
+    private final ProductQueryElasticsearchRepository elasticsearchRepository;
 
     @Override
     public Optional<ProductView> findById(UUID id) {
-        return jpaRepository.findById(id);
+        return elasticsearchRepository.findById(id);
     }
 
     @Override
     public ProductView save(ProductView productView) {
-        return jpaRepository.save(productView);
-    }
-
-    @Override
-    public void delete(ProductView productView) {
-        jpaRepository.delete(productView);
-    }
-
-    @Override
-    public Page<ProductView> search(ProductSearch productSearch, Pageable pageable) {
-        return jpaRepository.search(productSearch, pageable);
+        return elasticsearchRepository.save(productView);
     }
 }
--- product-service/src/main/java/com/destiny/productservice/presentation/dto/request/UpdateProductRequest.java
+++ product-service/src/main/java/com/destiny/productservice/presentation/dto/request/UpdateProductRequest.java
@@ -1,10 +1,8 @@
 package com.destiny.productservice.presentation.dto.request;
 
 import com.destiny.productservice.domain.entity.ProductStatus;
-import java.util.UUID;
 
 public record UpdateProductRequest(
-    UUID id,
     String name,
     Long price,
     String brand,
