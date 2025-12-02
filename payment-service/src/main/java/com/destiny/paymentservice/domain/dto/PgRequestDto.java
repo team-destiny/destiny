1fe68fbc21a734759a97a864eae5d850645138e0
believeme <cain1227@naver.com>
2025-12-02 12:50:25 +0900

    request 검증로직 추가

--- payment-service/src/main/java/com/destiny/paymentservice/domain/dto/PgRequestDto.java
+++ payment-service/src/main/java/com/destiny/paymentservice/domain/dto/PgRequestDto.java
@@ -1,12 +1,23 @@
 package com.destiny.paymentservice.domain.dto;
 
 import com.destiny.paymentservice.domain.vo.PaymentType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record PgRequestDto(
+    @NotBlank(message = "주문 번호는 필수 항목입니다.")
     String orderId,         // 결제할 주문 번호
-    Long totalAmount,            // 결제 금액
+
+    @NotNull(message = "결제 금액은 필수입니다.")
+    @PositiveOrZero(message = "결제 금액은 0과 같거나 커야 합니다.")
+    Long totalAmount,       // 결제 금액
+
+    @NotNull(message = "PG사 유형은 필수입니다.")
     PaymentType paymentType,     // 요청할 PG사 유형
-    String paymentKey,
+
+    String paymentKey, // 토스페이먼츠 필수 키
+
     String customerEmail,   // 고객 이메일
     String customerName,    // 고객 이름
     String customerMobilePhone // 고객 전화번호
--- payment-service/src/main/java/com/destiny/paymentservice/infrastructure/tosspayments/dto/request/TossConfirmRequest.java
+++ payment-service/src/main/java/com/destiny/paymentservice/infrastructure/tosspayments/dto/request/TossConfirmRequest.java
@@ -1,8 +1,18 @@
 package com.destiny.paymentservice.infrastructure.tosspayments.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record TossConfirmRequest(
+    @NotBlank(message = "결제 키는 필수입니다.")
     String paymentKey, // 결제 위젯을 누르면 생성되는 필수 값
+
+    @NotBlank(message = "주문 번호는 필수입니다.")
     String orderId,
+
+    @NotNull(message = "금액은 필수입니다.")
+    @PositiveOrZero(message = "결제 금액은 0과 같거나 커야 합니다.")
     Long amount
 ) {
 
