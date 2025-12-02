d92f886af70fc89526ddbffcc9a2e8a12e5ae96a
huhsiyoung <july433@naver.com>
2025-12-02 13:24:50 +0900

    fix: 데이터소스 설정 오류 수정

--- product-service/src/main/java/com/destiny/productservice/infrastructure/config/CommandDatabaseConfig.java
+++ product-service/src/main/java/com/destiny/productservice/infrastructure/config/CommandDatabaseConfig.java
@@ -3,6 +3,7 @@ package com.destiny.productservice.infrastructure.config;
 import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -29,15 +31,22 @@ public class CommandDatabaseConfig {
 
     @Bean
     public LocalContainerEntityManagerFactoryBean commandEntityManagerFactory(
-        EntityManagerFactoryBuilder builder) {
-        return builder
-            .dataSource(commandDataSource())
-            .packages("com.destiny.productservice.infrastructure.repository.command")
-            .build();
+        @Qualifier("commandDataSource") DataSource dataSource,
+        JpaVendorAdapter jpaVendorAdapter,
+        JpaProperties jpaProperties
+    ) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setPackagesToScan("com.destiny.productservice.domain.entity");
+        emf.setJpaVendorAdapter(jpaVendorAdapter);
+        emf.setJpaPropertyMap(jpaProperties.getProperties());
+        emf.setPersistenceUnitName("command");
+
+        return emf;
     }
 
     @Bean
-    public PlatformTransactionManager queryTransactionManager(
+    public PlatformTransactionManager commandTransactionManager(
         @Qualifier("commandEntityManagerFactory") EntityManagerFactory emf) {
         return new JpaTransactionManager(emf);
     }
