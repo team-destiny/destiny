fba90a00fd17e3f4dd04a8adeb475714dfbd73f9
changhui98 <net_0102@naver.com>
2025-12-02 21:34:11 +0900

    Feat : 브랜드(업체) 전용 주문 내역 조회 기능 구현

---
+++ order-service/src/main/java/com/destiny/orderservice/presentation/controller/InternalController.java
@@ -0,0 +1,33 @@
+package com.destiny.orderservice.presentation.controller;
+
+import com.destiny.orderservice.application.service.OrderService;
+import com.destiny.orderservice.presentation.dto.response.OrderItemForBrandResponse;
+import com.destiny.orderservice.presentation.dto.response.OrderItemResponse;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/v1/orders")
+@RequiredArgsConstructor
+public class InternalController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/items")
+    public ResponseEntity<List<OrderItemForBrandResponse>> getItemsForBrand(
+        @RequestParam UUID brandId
+    ) {
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(orderService.getItemsForBrand(brandId));
+    }
+
+}
