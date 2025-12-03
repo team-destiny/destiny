1687d300b72c6888f8a0c532c892887eca1d4121
jieun <dlwldms0895@gmail.com>
2025-12-04 04:49:15 +0900

    refactor : 유저 응답 코드 폴더 위치 변경

---
+++ user-service/src/main/java/com/destiny/userservice/presentation/advice/UserSuccessCode.java
@@ -0,0 +1,16 @@
+package com.destiny.userservice.presentation.advice;
+
+import com.destiny.global.code.ResponseCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserSuccessCode implements ResponseCode {
+    LOGOUT(HttpStatus.OK, "USER-SUCCESS-001", "모든 기기에서 로그아웃되었습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
