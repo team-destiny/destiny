ff5eeb3018cbdd66bd35206774f9f260e854d5a7
huhsiyoung <july433@naver.com>
2025-12-03 14:55:29 +0900

    feat: JpaConfig 추가

--- product-service/src/main/java/com/destiny/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/destiny/productservice/ProductServiceApplication.java
@@ -6,10 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableJpaRepositories(
-    basePackages = "com.destiny.productservice.infrastructure.repository.command"
-)
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
