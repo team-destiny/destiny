51ba4f3d4391a6b6b4ecba26ca558525d4e33a91
changhui98 <net_0102@naver.com>
2025-12-03 23:33:35 +0900

    Feat : 주문 취소 로직 추가

--- order-service/src/main/java/com/destiny/orderservice/presentation/controller/OrderController.java
+++ order-service/src/main/java/com/destiny/orderservice/presentation/controller/OrderController.java
@@ -6,7 +6,9 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,5 +38,23 @@ public class OrderController {
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(orderService.getOrderDetail(orderId));
+    }
+
+    @PatchMapping("/{orderId}/cancel")
+    public ResponseEntity<UUID> cancelOrder(
+        @PathVariable("orderId") UUID orderId
+    ) {
+        UUID order = orderService.cancelOrder(orderId);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(order);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<String> deleteOrder(
+        @PathVariable("orderId")  UUID orderId
+    ) {
+        return null;
     }
 }
