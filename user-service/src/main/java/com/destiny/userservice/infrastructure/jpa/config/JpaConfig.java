e463094f9970b779d1e3d06719c26603197230ee
jieun <dlwldms0895@gmail.com>
2025-12-03 16:44:47 +0900

    feat : security 템플릿 적용

--- user-service/src/main/java/com/destiny/userservice/infrastructure/jpa/config/JpaConfig.java
+++ user-service/src/main/java/com/destiny/userservice/infrastructure/jpa/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.destiny.userservice.infrastructure.config.jpa;
+package com.destiny.userservice.infrastructure.jpa.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
---
+++ user-service/src/main/java/com/destiny/userservice/infrastructure/security/auth/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.destiny.userservice.infrastructure.security.auth;
+
+import com.destiny.global.code.CommonErrorCode;
+import com.destiny.global.response.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().print(objectMapper.writeValueAsString(
+            ApiResponse.error(CommonErrorCode.UNAUTHORIZED)
+        ));
+    }
+}
