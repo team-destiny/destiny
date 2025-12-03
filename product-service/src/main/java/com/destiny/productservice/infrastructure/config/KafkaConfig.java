4d1578162f723047483263a8493863fcacbcf68b
huhsiyoung <july433@naver.com>
2025-12-03 10:46:10 +0900

    chore: Kafka producer 멱등성 활성화

--- product-service/src/main/java/com/destiny/productservice/infrastructure/config/KafkaConfig.java
+++ product-service/src/main/java/com/destiny/productservice/infrastructure/config/KafkaConfig.java
@@ -41,6 +41,7 @@ public class KafkaConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverHost);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
 
         return new DefaultKafkaProducerFactory<>(configProps);
     }
