b191b24214c7c2722eff3756f7267fac7bbb30dd
wnsgud0310 <ffjjo0310@naver.com>
2025-12-05 20:27:46 +0900

    feat: 발급된 쿠폰 jwt 기반 인증 구현

--- coupon-service/src/main/java/com/destiny/couponservice/presentation/controller/IssuedCouponController.java
+++ coupon-service/src/main/java/com/destiny/couponservice/presentation/controller/IssuedCouponController.java
@@ -2,6 +2,7 @@ package com.destiny.couponservice.presentation.controller;
 
 import com.destiny.couponservice.application.service.IssuedCouponService;
 import com.destiny.couponservice.domain.enums.IssuedCouponStatus;
+import com.destiny.couponservice.infrastructure.security.util.SecurityUtils;
 import com.destiny.couponservice.presentation.dto.request.CouponCancelRequest;
 import com.destiny.couponservice.presentation.dto.request.CouponUseRequest;
 import com.destiny.couponservice.presentation.dto.response.CouponUseResponse;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
+@PreAuthorize("isAuthenticated()")
 public class IssuedCouponController {
 
     private final IssuedCouponService issuedCouponService;
@@ -34,9 +37,9 @@ public class IssuedCouponController {
      */
     @PostMapping("/coupons/{couponTemplateId}/issue")
     public ResponseEntity<IssuedCouponResponseDto> issueCoupon(
-        @PathVariable UUID couponTemplateId,
-        @RequestHeader("X-USER-ID") UUID userId   // TODO: 유저 서비스 완성 되면 수정
+        @PathVariable UUID couponTemplateId
     ) {
+        UUID userId = SecurityUtils.getCurrentUserId();
         IssuedCouponResponseDto response = issuedCouponService.issueCoupon(userId,
             couponTemplateId);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -47,35 +50,37 @@ public class IssuedCouponController {
      */
     @GetMapping("/issued-coupons/{issuedCouponId}")
     public ResponseEntity<IssuedCouponResponseDto> getIssuedCoupon(
-        @PathVariable UUID issuedCouponId,
-        @RequestHeader("X-USER-ID") UUID userId   // TODO: 유저 서비스 완성 되면 수정
+        @PathVariable UUID issuedCouponId
     ) {
+        UUID userId = SecurityUtils.getCurrentUserId();
         IssuedCouponResponseDto response = issuedCouponService.getIssuedCoupon(userId,
             issuedCouponId);
         return ResponseEntity.ok(response);
     }
 
+
     /**
-     * 내가 발급받은 쿠폰 목록 조회 GET /v1/issued-coupons?status=AVAILABLE ( 사용가능한 쿠폰 목록조회 )
+     * 내가 발급받은 쿠폰 목록 조회 GET /v1/issued-coupons?status=AVAILABLE
      */
     @GetMapping("/issued-coupons")
     public ResponseEntity<IssuedCouponSearchResponse> getIssuedCoupons(
-        @RequestHeader("X-USER-ID") UUID userId,  // TODO: 유저 서비스 완성 되면 수정
         @RequestParam(defaultValue = "AVAILABLE") IssuedCouponStatus status,
         Pageable pageable
     ) {
+        UUID userId = SecurityUtils.getCurrentUserId();
         IssuedCouponSearchResponse response =
             issuedCouponService.getIssuedCoupons(userId, status, pageable);
 
         return ResponseEntity.ok(response);
     }
 
+
     @PostMapping("/issued-coupons/{issuedCouponId}/use")
     public ResponseEntity<CouponUseResponse> useCoupon(
         @PathVariable UUID issuedCouponId,
-        @RequestHeader("X-USER-ID") UUID userId,
         @Valid @RequestBody CouponUseRequest request
     ) {
+        UUID userId = SecurityUtils.getCurrentUserId();
         CouponUseResponse response = issuedCouponService.useCoupon(
             userId,
             issuedCouponId,
@@ -89,9 +94,9 @@ public class IssuedCouponController {
     @PostMapping("/issued-coupons/{issuedCouponId}/cancel")
     public ResponseEntity<Void> cancelCouponUse(
         @PathVariable UUID issuedCouponId,
-        @RequestHeader("X-USER-ID") UUID userId,
         @Valid @RequestBody CouponCancelRequest request
     ) {
+        UUID userId = SecurityUtils.getCurrentUserId();
         issuedCouponService.cancelCouponUse(userId, issuedCouponId, request.getOrderId());
         return ResponseEntity.noContent().build();
     }
