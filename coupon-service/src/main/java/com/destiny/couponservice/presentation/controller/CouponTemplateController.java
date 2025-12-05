c7b0da757bc85f65d145b473852133956a2145d2
wnsgud0310 <ffjjo0310@naver.com>
2025-12-05 19:18:15 +0900

    feat: 쿠폰템플릿 관리자 제한

--- coupon-service/src/main/java/com/destiny/couponservice/presentation/controller/CouponTemplateController.java
+++ coupon-service/src/main/java/com/destiny/couponservice/presentation/controller/CouponTemplateController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/coupon-templates")
+@PreAuthorize("hasRole('MASTER')")
 public class CouponTemplateController {
 
     private final CouponTemplateService couponTemplateService;
