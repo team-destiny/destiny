414312a3bb9d1266146dab0591a7f0d380f8fb44
believeme <cain1227@naver.com>
2025-12-05 10:05:42 +0900

    - amount를 0으로 초기화하지 않고 데이터륿 보존하도록 수정 - 주석 수정

--- payment-service/src/main/java/com/destiny/paymentservice/presentation/dto/request/PaymentRequest.java
+++ payment-service/src/main/java/com/destiny/paymentservice/presentation/dto/request/PaymentRequest.java
@@ -7,16 +7,15 @@ import java.util.UUID;
 
 /**
  * 결제 생성 요청 DTO (PENDING 상태 생성)
- * orderId는 UUID를 String으로 처리하여 유효성 검사를 합니다.
  */
 public record PaymentRequest(
     @NotNull(message = "주문 번호는 필수입니다.")
     UUID orderId,
 
-    // PG사 거래 번호는 MOCK 결제 시 null이 될 수 있으므로 @NotBlank를 사용하지 않음
+    // PG사 거래 번호는 MOCK 결제 시 null이 될 수 있도록 설정
     String pgTxId,
 
-    // PaymentType은 MOCK 결제 시 null이 될 수 있으므로 @NotNull을 사용하지 않음
+    // PaymentType은 MOCK 결제 시 null이 될 수 있도록 설정
     PaymentType paymentType,
 
     @NotNull(message = "결제 금액은 필수입니다.")
