6c25f7fc9282cd2611c735eb3286814cf37371fd
changhui98 <net_0102@naver.com>
2025-12-05 16:16:13 +0900

    Feat : 상품 서비스 검증 성공 시 실행 로직 초기 세팅

--- saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/infrastructure/repository/SagaRepositoryImpl.java
+++ saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/infrastructure/repository/SagaRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.destiny.sagaorchestrator.infrastructure.repository;
 
 import com.destiny.sagaorchestrator.domain.entity.SagaState;
 import com.destiny.sagaorchestrator.domain.repository.SagaRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +16,11 @@ public class SagaRepositoryImpl implements SagaRepository {
     public void createSaga(SagaState saga) {
 
         sagaJpaRepository.save(saga);
+    }
+
+    @Override
+    public SagaState findByOrderId(UUID orderId) {
+
+        return sagaJpaRepository.findByOrderId(orderId);
     }
 }
