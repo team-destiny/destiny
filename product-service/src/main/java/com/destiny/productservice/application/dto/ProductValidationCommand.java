97e41cf030939c90b94717cdfc5be62ff84429b0
huhsiyoung <july433@naver.com>
2025-12-05 14:22:09 +0900

    refactor: 상품 검증 요청 커맨드, 이벤트 메세지에 orderId 추가

--- product-service/src/main/java/com/destiny/productservice/application/dto/ProductValidationCommand.java
+++ product-service/src/main/java/com/destiny/productservice/application/dto/ProductValidationCommand.java
@@ -4,5 +4,6 @@ import java.util.List;
 import java.util.UUID;
 
 public record ProductValidationCommand(
+    UUID orderId,
     List<UUID> productIds
 ) { }
--- product-service/src/main/java/com/destiny/productservice/application/dto/ProductValidationFail.java
+++ product-service/src/main/java/com/destiny/productservice/application/dto/ProductValidationFail.java
@@ -1,7 +1,9 @@
 package com.destiny.productservice.application.dto;
 
 import java.util.List;
+import java.util.UUID;
 
 public record ProductValidationFail (
+    UUID orderId,
     List<ProductFailDetail> productFailDetails
 ) { }
