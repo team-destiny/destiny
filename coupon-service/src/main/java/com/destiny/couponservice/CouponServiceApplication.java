82dc3b35320ff1aae4ec99166ad15c4dd6f7716f
wnsgud0310 <ffjjo0310@naver.com>
2025-12-08 01:19:53 +0900

    fix: 패키지 스캔 제거

--- coupon-service/src/main/java/com/destiny/couponservice/CouponServiceApplication.java
+++ coupon-service/src/main/java/com/destiny/couponservice/CouponServiceApplication.java
@@ -3,7 +3,7 @@ package com.destiny.couponservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.destiny")
+@SpringBootApplication
 public class CouponServiceApplication {
 
 	public static void main(String[] args) {
