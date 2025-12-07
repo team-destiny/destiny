c07eb65096d6b398d1f8c53028bb4de861d03f98
huhsiyoung <july433@naver.com>
2025-12-08 01:40:22 +0900

    feat: 재고 차감, 롤백 기능 구현

---
+++ stock-service/src/main/java/com/destiny/stockservice/domain/entity/Stock.java
@@ -0,0 +1,39 @@
+package com.destiny.stockservice.domain.entity;
+
+import com.destiny.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Version;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stock extends BaseEntity {
+
+    @Id
+    private UUID id;
+
+    private UUID productId;
+
+    private Integer quantity;
+
+    @Version
+    private Long version;
+
+    public boolean tryDecrease(Integer amount) {
+        if (quantity < amount) {
+            return false;
+        }
+        quantity -= amount;
+        return true;
+    }
+
+    public void increase(Integer amount) {
+        quantity += amount;
+    }
+}
---
+++ stock-service/src/main/java/com/destiny/stockservice/infrastructure/repository/StockRepositoryImpl.java
@@ -0,0 +1,19 @@
+package com.destiny.stockservice.infrastructure.repository;
+
+import com.destiny.stockservice.domain.entity.Stock;
+import com.destiny.stockservice.domain.repository.StockRepository;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class StockRepositoryImpl implements StockRepository {
+
+    private final StockJpaRepository stockJpaRepository;
+
+    @Override
+    public Stock findByProductId(UUID productId) {
+        return stockJpaRepository.findByProductId(productId);
+    }
+}
