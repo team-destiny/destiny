06dd1134dae92a59c65c4a822f9d61933ca747af
jieun <dlwldms0895@gmail.com>
2025-12-08 16:15:34 +0900

    refactor : 오류코드 변경(UNAUTHORIZE > FORBIDDEN)

--- review-service/src/main/java/com/destiny/reviewservice/presentation/advice/ReviewErrorCode.java
+++ review-service/src/main/java/com/destiny/reviewservice/presentation/advice/ReviewErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum ReviewErrorCode implements ResponseCode {
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW_ERROR_001", "리뷰를 찾을 수 없습니다."),
     INVALID_SEARCH_CONDITION(HttpStatus.BAD_REQUEST, "REVIEW_ERROR_002", "productId와 userId는 동시에 사용할 수 없습니다."),
-    USER_NOT_ORDER_OWNER(HttpStatus.UNAUTHORIZED, "REVIEW_ERROR_003", "주문한 사용자만 리뷰를 작성할 수 있습니다."),
+    USER_NOT_ORDER_OWNER(HttpStatus.FORBIDDEN, "REVIEW_ERROR_003", "주문한 사용자만 리뷰를 작성할 수 있습니다."),
     ORDER_NOT_REVIEWABLE(HttpStatus.BAD_REQUEST, "REVIEW_ERROR_004", "아직 리뷰를 작성할 수 없는 주문 상태입니다."),
     PRODUCT_NOT_IN_ORDER(HttpStatus.BAD_REQUEST, "REVIEW_ERROR_005", "해당 상품은 주문 내역에 포함되어 있지 않습니다."),
     REVIEW_ALREADY_EXISTS(HttpStatus.CONFLICT, "REVIEW_ERROR_006", "이미 리뷰를 작성한 주문/상품입니다."),
