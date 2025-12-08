9fe7825c846ba9b1d9d6e20ac0f11972dfd683fc
jieun <dlwldms0895@gmail.com>
2025-12-08 12:18:36 +0900

    fix : 패키지 경로 변경 (example -> destiny)

---
+++ review-service/src/main/java/com/destiny/reviewservice/domain/repository/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.destiny.reviewservice.domain.repository;
+
+import com.destiny.reviewservice.domain.entity.Review;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ReviewRepository {
+    Review save(Review review);
+    Review findById(UUID reviewId);
+    Review findByReviewIdAndDeletedAtIsNull(UUID reviewId);
+    Page<Review> findByProductIdAndDeletedAtIsNull(UUID productId, Pageable pageable);
+    Page<Review> findByUserIdAndDeletedAtIsNull(UUID userId, Pageable pageable);
+}
