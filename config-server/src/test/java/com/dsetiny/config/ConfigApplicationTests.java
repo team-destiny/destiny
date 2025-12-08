5fd13921111432991e3fe785059349372607cd47
jieun <dlwldms0895@gmail.com>
2025-12-08 16:18:45 +0900

    feat : config-server init

---
+++ config-server/src/test/java/com/dsetiny/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsetiny.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
