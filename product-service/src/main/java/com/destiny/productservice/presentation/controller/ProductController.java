d51dcc35f3f88326b320c6c0dbc2566fbbc52695
huhsiyoung <july433@naver.com>
2025-12-08 15:51:58 +0900

    fix: brandId null 체크 추가로 검색 쿼리 오류 해결

--- product-service/src/main/java/com/destiny/productservice/presentation/controller/ProductController.java
+++ product-service/src/main/java/com/destiny/productservice/presentation/controller/ProductController.java
@@ -8,6 +8,7 @@ import com.destiny.productservice.presentation.dto.request.UpdateProductRequest;
 import com.destiny.productservice.presentation.dto.response.ProductResponse;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/products")
@@ -39,6 +41,8 @@ public class ProductController {
         @RequestParam(value = "size", required = false) String size,
         @RequestParam(value = "color", required = false) String color,
         Pageable pageable) {
+
+        log.info("request accepted");
 
         Page<ProductResponse> pages = productQueryService.getProducts(
                 ProductSearch.of(minPrice, maxPrice, nameContains, brandId, size, color),
