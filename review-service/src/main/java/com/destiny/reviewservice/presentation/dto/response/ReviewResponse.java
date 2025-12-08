5504aa90a64f114dfd49ebd4e1cdda6cd10d8b98
jieun <dlwldms0895@gmail.com>
2025-12-08 16:21:54 +0900

    fix : review 반환형 변경 (string > uuid)

--- review-service/src/main/java/com/destiny/reviewservice/presentation/dto/response/ReviewResponse.java
+++ review-service/src/main/java/com/destiny/reviewservice/presentation/dto/response/ReviewResponse.java
@@ -2,19 +2,20 @@ package com.destiny.reviewservice.presentation.dto.response;
 
 import com.destiny.reviewservice.domain.entity.Review;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record ReviewResponse (
-    String reviewId,
-    String userId,
-    String productId,
-    String orderId,
+    UUID reviewId,
+    UUID userId,
+    UUID productId,
+    UUID orderId,
     int rating,
     String content,
     String imageUrls,
     LocalDateTime createdAt,
-    Long createdBy,
+    UUID createdBy,
     LocalDateTime updatedAt,
-    Long updatedBy
+    UUID updatedBy
 ){
 
     public static ReviewResponse of(Review review) {
