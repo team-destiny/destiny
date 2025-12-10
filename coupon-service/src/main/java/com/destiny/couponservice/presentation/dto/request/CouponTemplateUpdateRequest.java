5598476b42c83c634c99466e352a1c65d3bacde1
wnsgud0310 <ffjjo0310@naver.com>
2025-12-11 00:02:21 +0900

    refactor: 쿠폰 템플릿 수정 API Patch -> PUT으로 변경

--- coupon-service/src/main/java/com/destiny/couponservice/presentation/dto/request/CouponTemplateUpdateRequest.java
+++ coupon-service/src/main/java/com/destiny/couponservice/presentation/dto/request/CouponTemplateUpdateRequest.java
@@ -2,6 +2,8 @@ package com.destiny.couponservice.presentation.dto.request;
 
 import com.destiny.couponservice.domain.enums.DiscountType;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -13,27 +15,36 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CouponTemplateUpdateRequest {
 
+    @NotBlank
     private String name;
 
+    @NotNull
     private DiscountType discountType;
 
+    @NotNull
     private Integer discountValue;
 
+    @NotNull
     @PositiveOrZero
     private Integer minOrderAmount;
 
+    @NotNull
     private Boolean isDuplicateUsable;
 
+    @NotNull
     private Integer maxDiscountAmount;
 
+    @NotNull
     private Integer dailyIssueLimit;
 
+    @NotNull
     private Integer perUserTotalLimit;
 
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime availableFrom;
 
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime availableTo;
-
 }
