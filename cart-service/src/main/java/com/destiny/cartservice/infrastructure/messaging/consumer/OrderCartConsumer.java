fc4f17f6b245a4bc344d053cdb559a97420605b6
gahyeon <janghyeon2412@gmail.com>
2025-12-11 12:04:55 +0900

    Feat: Kafka 장바구니 비우기(clearCart) 연동
    - cart-clear-request kafka 메시지 처리 로직
    - cartId 기반 장바구니 삭제 기능 적용
    - CartService/Impl 로직 정리 및 보완
    - 관련 DTO 및 Controller 구조 점검

---
+++ cart-service/src/main/java/com/destiny/cartservice/infrastructure/messaging/consumer/OrderCartConsumer.java
@@ -0,0 +1,34 @@
+package com.destiny.cartservice.infrastructure.messaging.consumer;
+
+import com.destiny.cartservice.application.dto.event.CartClearEvent;
+import com.destiny.cartservice.application.service.CartService;
+import com.destiny.cartservice.presentation.dto.request.CartDeleteRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderCartConsumer {
+
+    private final CartService cartService;
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "cart-clear-request", groupId = "saga-orchestrator")
+    public void handleCartClearEvent(String message) {
+
+        try{
+            CartClearEvent event = objectMapper.readValue(message, CartClearEvent.class);
+
+            cartService.clearCart(event);
+        } catch (JsonProcessingException e) {
+
+            log.error("[Kafka] 장바구니 비우기 중 에러", e);
+        }
+    }
+}
