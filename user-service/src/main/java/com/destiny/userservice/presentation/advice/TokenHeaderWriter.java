d6bf70725cf715876fbc697f5b69685e4380196a
jieun <dlwldms0895@gmail.com>
2025-12-12 15:21:24 +0900

    refactor : refreshToken header에 저장 (쿠키 저장 로직 수정, 확장성 고려)

--- user-service/src/main/java/com/destiny/userservice/presentation/advice/TokenHeaderWriter.java
+++ user-service/src/main/java/com/destiny/userservice/presentation/advice/TokenHeaderWriter.java
@@ -1,14 +1,13 @@
 package com.destiny.userservice.presentation.advice;
 
 import com.destiny.userservice.infrastructure.security.jwt.JwtProperties;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-// refreshToken HttpOnly 쿠키에 저장
+// refreshToken 헤더에 저장
 public class TokenHeaderWriter {
 
     private final JwtProperties jwtProperties;
@@ -19,21 +18,23 @@ public class TokenHeaderWriter {
     }
 
     public void refreshTokenWrite(HttpServletResponse servletResponse, String refreshToken) {
-        Cookie cookie = new Cookie(jwtProperties.getRefreshCookieName(), refreshToken);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(jwtProperties.isRefreshCookieSecure());
-        cookie.setPath("/");
-        cookie.setMaxAge((int) (jwtProperties.getRefreshExpirationMillis() / 1000));
+//        Cookie cookie = new Cookie(jwtProperties.getRefreshCookieName(), refreshToken);
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(jwtProperties.isRefreshCookieSecure());
+//        cookie.setPath("/");
+//        cookie.setMaxAge((int) (jwtProperties.getRefreshExpirationMillis() / 1000));
+//
+//        servletResponse .addCookie(cookie);
 
-        servletResponse .addCookie(cookie);
+        servletResponse.setHeader(jwtProperties.getRefreshHeaderName(), refreshToken);
     }
 
-    public void expire(HttpServletResponse response) {
-        Cookie cookie = new Cookie(jwtProperties.getRefreshCookieName(), "");
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(0);  // 바로 만료
-        response.addCookie(cookie);
-    }
+//    public void expire(HttpServletResponse response) {
+//        Cookie cookie = new Cookie(jwtProperties.getRefreshHeaderName(), "");
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
+//        cookie.setPath("/");
+//        cookie.setMaxAge(0);  // 바로 만료
+//        response.addCookie(cookie);
+//    }
 }
