cd6c24871334275d5b38b2d1cadcc54849bd76a7
jieun <dlwldms0895@gmail.com>
2025-12-12 14:34:16 +0900

    refactor : user 정렬 createdBy로 고정

--- user-service/src/main/java/com/destiny/userservice/presentation/advice/PageingUtils.java
+++ user-service/src/main/java/com/destiny/userservice/presentation/advice/PageingUtils.java
@@ -9,7 +9,7 @@ public class PageingUtils {
     private static final int[] ALLOWED_SIZES = {10, 30, 50};
     private static final int DEFAULT_OFFSET = 0;
 
-    public static Pageable createPageable(int size, String sortBy, boolean isDescending) {
+    public static Pageable createPageable(int page, int size, boolean isDescending) {
 
         // 페이징 정책
         if (!isAllowedSize(size)) {
@@ -17,9 +17,9 @@ public class PageingUtils {
         }
 
         // 정렬 정책
-        Sort sort = createSort(sortBy,isDescending);
+        Sort sort = createSort(isDescending);
 
-        return PageRequest.of(DEFAULT_OFFSET, size, sort);
+        return PageRequest.of(page, size, sort);
     }
 
     // 페이징 정책. 10, 30, 50 단위로 페이징 가능
@@ -31,11 +31,8 @@ public class PageingUtils {
     }
 
     // 정렬 정책. 기본값: 생성자, 내림차순
-    private static Sort createSort(String sortBy, boolean isDescending) {
+    private static Sort createSort(boolean isDescending) {
         String field = "createdAt";
-        if (sortBy != null && sortBy.equals("updatedAt")) {
-            field = "updatedAt";
-        }
         return Sort.by(isDescending ? Sort.Direction.DESC : Sort.Direction.ASC, field);
     }
 }
