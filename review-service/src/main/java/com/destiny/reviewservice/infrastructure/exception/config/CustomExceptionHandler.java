9aaa955314a55f3ceed68f4ce339f9cb84124a1d
jieun <dlwldms0895@gmail.com>
2025-12-16 02:33:22 +0900

    fix : review 예외 처리 핸들러 추가

---
+++ review-service/src/main/java/com/destiny/reviewservice/infrastructure/exception/config/CustomExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.destiny.reviewservice.infrastructure.exception.config;
+
+import com.destiny.global.exception.GlobalExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler extends GlobalExceptionHandler {
+
+}
