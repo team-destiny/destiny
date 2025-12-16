cacaa7f05620f5cec3fc487d3bedb3857e2d77d4
huhsiyoung <july433@naver.com>
2025-12-16 19:18:38 +0900

    chore: 사가 이벤트명 StockReduce을 StockReservation으로 변경

--- saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/infrastructure/messaging/event/result/StockReservationSuccessResult.java
+++ saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/infrastructure/messaging/event/result/StockReservationSuccessResult.java
@@ -3,9 +3,9 @@ package com.destiny.sagaorchestrator.infrastructure.messaging.event.result;
 import java.util.List;
 import java.util.UUID;
 
-public record StockReduceSuccessResult(
+public record StockReservationSuccessResult(
     UUID orderId,
-    List<StockReduceItem> items
+    List<StockReservationItem> items
 ) {
 
 }
