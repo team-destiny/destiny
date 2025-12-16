74168c2ee8b343b522027bbb8b6a36ba9c2c2668
huhsiyoung <july433@naver.com>
2025-12-16 20:34:46 +0900

    fix: findAllByOrderId 쿼리 메서드 오타 수정

--- stock-service/src/main/java/com/destiny/stockservice/infrastructure/repository/StockReservationJpaRepository.java
+++ stock-service/src/main/java/com/destiny/stockservice/infrastructure/repository/StockReservationJpaRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StockReservationJpaRepository extends JpaRepository<StockReservation, UUID> {
 
-    List<StockReservation> findALlByOrderId(UUID orderId);
+    List<StockReservation> findAllByOrderId(UUID orderId);
 }
