ce20903b07405575f1a23c11775fb6003298f972
huhsiyoung <july433@naver.com>
2025-12-18 11:13:55 +0900

    fix: 재고 취소시 이미 취소된 재고일 때 리턴값 수정

--- stock-service/src/main/java/com/destiny/stockservice/domain/result/ConfirmedStockCancelResult.java
+++ stock-service/src/main/java/com/destiny/stockservice/domain/result/ConfirmedStockCancelResult.java
@@ -10,7 +10,8 @@ public enum ConfirmedStockCancelResult {
     CANCEL_SUCCEEDED("재고 취소 요청을 성공했습니다."),
     INVALID_REQUEST("잘못된 재고 취소 요청값입니다."),
     NO_RESERVATION("취소할 재고 확정 건이 없습니다."),
-    CANCEL_FAILED("재고 취소 요청에 실패했습니다.");
+    CANCEL_FAILED("재고 취소 요청에 실패했습니다."),
+    ALREADY_CANCELED("이미 취소된 재고입니다.");
 
     private final String description;
 }
