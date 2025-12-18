f95d8bd682fd2c4fb65dbe48bb10bf3246dac8e5
changhui98 <net_0102@naver.com>
2025-12-18 15:40:46 +0900

    Feat : 사가 서비스 시큐리티 적용

---
+++ saga-orchestrator/src/main/java/com/destiny/sagaorchestrator/infrastructure/config/CustomExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.destiny.sagaorchestrator.infrastructure.config;
+
+import com.destiny.global.exception.GlobalExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler extends GlobalExceptionHandler {
+
+}
