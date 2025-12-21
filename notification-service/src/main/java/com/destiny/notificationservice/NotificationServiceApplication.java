6ff0504cf21689d851a7e47e71c7796803cff332
gahyeon <janghyeon2412@gmail.com>
2025-12-21 20:55:23 +0900

    Refactor: 코드래빗 피드백 반영

--- notification-service/src/main/java/com/destiny/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/destiny/notificationservice/NotificationServiceApplication.java
@@ -1,8 +1,11 @@
 package com.destiny.notificationservice;
 
+import com.destiny.notificationservice.infrastructure.config.NotificationCacheRetryProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(NotificationCacheRetryProperties.class)
 @SpringBootApplication
 public class NotificationServiceApplication {
 
