b752c8e05052899f037034868b97d61df0737755
gahyeon <janghyeon2412@gmail.com>
2025-12-22 09:03:49 +0900

    Refactor: 장바구니 리팩토링

--- cart-service/src/main/java/com/destiny/cartservice/presentation/dto/request/CartDeleteRequest.java
+++ cart-service/src/main/java/com/destiny/cartservice/presentation/dto/request/CartDeleteRequest.java
@@ -1,5 +1,6 @@
 package com.destiny.cartservice.presentation.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CartDeleteRequest {
 
+    @NotEmpty(message = "삭제할 장바구니 ID 목록은 필수입니다")
     private List<UUID> cartIds;
 
 }
