3a76935602208039e4fcb305aec0d8fcfc72dfca
believeme <cain1227@naver.com>
2025-12-23 10:00:21 +0900

    이름 수정

--- payment-service/src/main/java/com/destiny/paymentservice/application/service/inter/PaymentService.java
+++ payment-service/src/main/java/com/destiny/paymentservice/application/service/inter/PaymentService.java
@@ -8,6 +8,6 @@ import com.destiny.paymentservice.presentation.dto.response.PaymentResponse;
 public interface PaymentService {
     // 이 서비스가 어떤 PG사를 지원하는지 반환 (TOSS, PORTONE 등)
     PaymentProvider supports();
-    PaymentResponse confirmPayment(PaymentConfirmRequest command);
-    PaymentResponse cancelPayment(PaymentCancelRequest command);
+    PaymentResponse confirmPayment(PaymentConfirmRequest request);
+    PaymentResponse cancelPayment(PaymentCancelRequest request);
 }
